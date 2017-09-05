1b2acf95085941e239e4e461bff5a0dd77cacc1d
sha <urabummer4@hotmail.com>
2017-09-06 01:14:46 +0800

    rails tutorial lesson 3

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
